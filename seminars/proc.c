791fde45c4242c11522f126f2767f68b8a43a088
Anastasiia <anastasia-tishchenko@mail.ru>
2025-03-18 09:25:56 +0300

    system processes

---
+++ seminars/proc.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/types.h>
+
+int main(void){
+    // system("sleep 5 &");
+    // printf("end-of-program\n");
+    // return 0;
+
+    // system("cd somewhere");
+    // return 0;
+
+    // int uid = getuid();
+    // struct passwd * pwdp = getpwuid(uid);
+    // if (pwdp == NULL){
+    //     fprintf()
+    // }
+
+    fork();
+    printf("Hello World!\n");
+    sleep(15);
+    return 0;
+
+}
